de4f62797352756503be2cd099ce1021234ac450
“ZEENIA <zeenia.haji@kcl.ac.uk>
2023-11-25 20:34:43 +0000

    update form

--- things/views.py
+++ things/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            pass
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
